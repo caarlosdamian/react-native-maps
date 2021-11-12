4c4dbec9e25593aacb67e149145b31e1d1c170e2
caarlosdamian <dapecarlos@gmail.com>
2021-11-11 23:20:25 -0600

    Add Draggeble to pin

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
-import * as React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import * as React from "react";
+import MapView, { Callout, Circle, Marker } from "react-native-maps";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 
 export default function App() {
+  const [pin, setPin] = React.useState({
+    latitude: 19.285013,
+    longitude: -103.7327,
+  });
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 19.2433,
+          longitude: -103.725,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        // provider="google"
+      >
+        <Marker
+          coordinate={pin}
+          draggable={true}
+          onDragStart={(e) => console.log(e.nativeEvent.coordinate)}
+          onDragEnd={(e) =>
+            setPin({
+              latitude: e.nativeEvent.coordinate.latitude,
+              longitude: e.nativeEvent.coordinate.longitude,
+            })
+          }
+        >
+          <Callout>
+            <Text>{JSON.stringify(pin)}</Text>
+          </Callout>
+        </Marker>
+        <Circle
+          center={pin}
+          radius={1000}
+        />
+      </MapView>
     </View>
   );
 }
@@ -13,12 +46,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
